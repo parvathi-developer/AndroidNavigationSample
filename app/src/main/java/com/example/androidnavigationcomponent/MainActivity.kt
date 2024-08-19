579a43ad09fd30648ce730eab0d43d1a96ebb894
parvathi-appdevloper <parvathi.appdevloper@gmail.com>
2024-08-19 13:13:02 +0530

    Full project on navigation

---
+++ app/src/main/java/com/example/androidnavigationcomponent/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.androidnavigationcomponent
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.androidnavigationcomponent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/example/androidnavigationcomponent/MainActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.androidnavigationcomponent
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel:ViewModel() {
+}
